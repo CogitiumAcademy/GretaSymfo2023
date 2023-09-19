2a66dd9218aea7c4744f2afb0c183a3d08550180
Cogitium <info@cogitium.com>
2023-09-19 17:02:28 +0200

    Intégration sbadmin, ajout/modification category, début suppression

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin/category', name: 'admin_category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        //dd($categories);
+
+        return $this->render('admin/category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/add', name: 'add')]
+    public function addCategory(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('admin/category/add.html.twig', [
+            'form' => $form->createView(),
+            'h1' => 'Ajouter une catégorie'
+        ]);
+    }
+
+    #[Route('/update/{id}', name: 'update')]
+    public function updateCategory(Category $category, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->flush();
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('admin/category/add.html.twig', [
+            'form' => $form->createView(),
+            'h1' => 'Modifier une catégorie'
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function deleteCategory(Category $category, Request $request, ManagerRegistry $doctrine): Response
+    {
+        return $this->redirectToRoute('admin_category_index');
+    }
+
+
+}
