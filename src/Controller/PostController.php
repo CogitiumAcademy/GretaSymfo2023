92311391d5162caa59a0603207890064164884d5
Cogitium <info@cogitium.com>
2023-09-19 12:51:44 +0200

    Réglage security; début intégration sbadmin

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
@@ -16,14 +17,19 @@ class PostController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(PostRepository $postRepository): Response
     {
-        $posts = $postRepository->findAll();
+        //$posts = $postRepository->findAll();
+        $posts = $postRepository->findLastPosts();
         //dd($posts);
+        
+        $oldPosts = $postRepository->findOldPosts();
 
         return $this->render('post/index.html.twig', [
             'posts' => $posts,
+            'oldPosts' => $oldPosts
         ]);
     }
 
+    #[IsGranted('ROLE_USER')]
     #[Route('/post/add', name: 'post_add')]
     public function addPost(Request $request, ManagerRegistry $doctrine): Response
     {
@@ -48,14 +54,32 @@ class PostController extends AbstractController
     #[Route('/post/{slug}', name: 'post_view')]
     public function post_view(Post $post): Response
     {
-        dd($post);
+        //dd($post);
 
         return $this->render('post/view.html.twig', [
-            'post' => [
-                'title' => 'Le super titre',
-                'content' => 'Le super contenu'
-            ],
+            'post' => $post,
         ]);
+    }
+
+    #[Route('/post2/{id}', name: 'post2_view')]
+    public function post_view2($id, PostRepository $postRepository): Response
+    {
+        //dd($id);
+        $post = $postRepository->find($id);
+        //dd($post);
+
+        return $this->render('post/view.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test(PostRepository $postRepository): Response
+    {
+        //$posts = $postRepository->findOldPosts();
+        //$posts = $postRepository->findLastPosts();
+        $counts = $postRepository->nbAllSubjects();
+        dd($counts);
     }
 
 }
